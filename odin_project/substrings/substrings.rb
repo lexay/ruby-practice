e55801153a9fcad5ec054e99c0038286b784da02
Alexey K <alexcrpts@gmail.com>
2022-11-06 23:32:36 +0300

    use include? to return boolean instead of truthy value

--- odin_project/substrings/substrings.rb
+++ odin_project/substrings/substrings.rb
@@ -2,7 +2,7 @@ def substrings(string, dictionary)
   words = string.scan /\w+\b/
   dictionary.inject(Hash.new(0)) do |hash, substring|
     words.each do |word|
-      hash[substring] += 1 if word.downcase[substring]
+      hash[substring] += 1 if word.downcase.include? substring
     end
     hash
   end
