99721cc7d1c91a750f367a7da2aef3b68b74591f
Alexey K <alexcrpts@gmail.com>
2022-12-14 00:02:56 +0300

    fixup! feat(odin, ttt): add tests for Game:Round

--- odin_project/ttt/spec/result_spec.rb
+++ odin_project/ttt/spec/result_spec.rb
@@ -1,13 +1,17 @@
 require_relative '../lib/round.rb'
 require_relative '../lib/board.rb'
 require_relative '../helpers/board_helper.rb'
+require_relative '../lib/player.rb'
 require 'spec_helper'
+
+p1 = TicTacToe::Player.new
+p2 = TicTacToe::Player.new
 
 RSpec.describe 'Player wins' do
   3.times do |idx|
     it "returns win if #{idx + 1}st row is won" do
       board = TicTacToe::Board.new
-      round = TicTacToe::Game::Round.new(board, 'p1', 'p2')
+      round = TicTacToe::Game::Round.new(board, p1, p2)
       board.table[idx] = %w[X X X]
       expect(round.instance_eval { check_combinations }).to eq('win')
     end
@@ -16,7 +20,7 @@ RSpec.describe 'Player wins' do
   3.times do |idx|
     it "returns win if #{idx + 1}st column is won" do
       board = TicTacToe::Board.new
-      round = TicTacToe::Game::Round.new(board, 'p1', 'p2')
+      round = TicTacToe::Game::Round.new(board, p1, p2)
       board.table.map { |row| row[idx] = 'X' }
       expect(round.instance_eval { check_combinations }).to eq('win')
     end
@@ -24,14 +28,14 @@ RSpec.describe 'Player wins' do
 
   it 'returns win if left diagonal is won' do
     board = TicTacToe::Board.new
-    round = TicTacToe::Game::Round.new(board, 'p1', 'p2')
+    round = TicTacToe::Game::Round.new(board, p1, p2)
     board.table.map.with_index { |row, idx| row[idx] = 'X' }
     expect(round.instance_eval { check_combinations }).to eq('win')
   end
 
   it 'returns win if right diagonal is won' do
     board = TicTacToe::Board.new
-    round = TicTacToe::Game::Round.new(board, 'p1', 'p2')
+    round = TicTacToe::Game::Round.new(board, p1, p2)
     board.table.reverse.map.with_index { |row, idx| row[idx] = 'X' }
     expect(round.instance_eval { check_combinations }).to eq('win')
   end
@@ -40,7 +44,7 @@ end
 RSpec.describe 'Players are drawn' do
   it 'returns draw if no free positions left' do
     board = TicTacToe::Board.new
-    round = TicTacToe::Game::Round.new(board, 'p1', 'p2')
+    round = TicTacToe::Game::Round.new(board, p1, p2)
     board.table.replace [%w[X O X], %w[O X O], %w[O X O]]
     expect(round.instance_eval { check_combinations }).to eq('draw')
   end
