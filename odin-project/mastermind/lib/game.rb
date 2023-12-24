ea866b02dca648a9dca7471d0963c4fb445c5ac6
Alexey K <alexcrpts@gmail.com>
2023-12-24 11:13:01 +0300

    fix(odin, mastermind): avoid introducing `Peg` on init

--- odin-project/mastermind/lib/game.rb
+++ odin-project/mastermind/lib/game.rb
@@ -6,16 +6,10 @@ module Mastermind
       attr_accessor :count
     end
 
-    attr_reader :code_pegs, :key_pegs
 
     def initialize
-      code_pegs = CodePeg.create_each(10)
-      key_pegs = KeyPeg.create_each(10)
-
       Game.count += 1
 
-      cm = CodeMaker.new
-      cb = CodeBreaker.new
 
       end
 
