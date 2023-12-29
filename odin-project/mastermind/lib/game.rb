c93a4ed82c891490c7e6cff0bfb33f32c401f15a
Alexey K <alexcrpts@gmail.com>
2023-12-29 17:28:47 +0300

    feat(odin, mastermind): create player

--- odin-project/mastermind/lib/game.rb
+++ odin-project/mastermind/lib/game.rb
@@ -8,6 +8,28 @@ module Mastermind
 
     def initialize
       Game.count += 1
+      run
+    end
+
+    def run
+      players = create_players
+    end
+
+    def create_players
+      players_menu = <<~MESSAGE
+        1. 1 Player
+        2. 2 Players
+      MESSAGE
+      puts players_menu
+      print 'Choose number of players: '
+      players_count = gets.to_i
+      puts
+
+      if players_count == 2
+        [HumanPlayer.new, HumanPlayer.new]
+      else
+        [HumanPlayer.new, CpuPlayer.new]
+      end
     end
   end
 end
